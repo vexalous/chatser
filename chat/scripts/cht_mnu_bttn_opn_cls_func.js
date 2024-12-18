546a3bc3285fe16208dc1b609007950f6cbb9c72
vexalous <vexalous@hotmail.com>
2024-12-18 13:24:59 -0500

    Update cht_mnu_bttn_opn_cls_func.js

--- chat/scripts/cht_mnu_bttn_opn_cls_func.js
+++ chat/scripts/cht_mnu_bttn_opn_cls_func.js
@@ -1,1 +1,8 @@
-
+function mnuinteract() {
+  var mnu = document.getElementById("cht_mnu_bttn");
+  if (cht_mnu_bttn.style.display === "none") {
+    cht_mnu_bttn.style.display = "block";
+  } else {
+    cht_mnu_bttn.style.display = "none";
+  }
+}
